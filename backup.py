c7f6c3231d93cb72d2702855612e39fb1a5b96f9
BDCash69 <79369161+BDCash69@users.noreply.github.com>
2024-08-19 10:58:42 +0100

    Update backup.py

--- backup.py
+++ backup.py
@@ -1,13 +1,8 @@
 import zipfile, os
-# from datetime import datetime
-# now=datetime.now().strftime('%Y-%m-%d %H.%M.%S')
 
 appdata = os.environ['APPDATA'].replace('\\Roaming','')
-# onedrive = os.environ['ONEDRIVE']
 source = os.path.join(appdata, "Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang/minecraftWorlds")
-# output_base = os.path.join(onedrive, 'Desktop/python/minecraft/')
 output_base = '//MORPHEUS/backup/minecraft/Minecraft worlds'
-
 
 def zip_folder(path_to_folder, output_path):
     with zipfile.ZipFile(output_path, 'w', zipfile.ZIP_DEFLATED) as zf:
@@ -26,13 +21,11 @@ def get_world_folder_paths(target_folder):
             folder_paths.append(entry_path)
     return folder_paths
 
-
-
 worlds = get_world_folder_paths(source)
 
 for world in worlds:
     counter = 0
-    # Read world name from txt file
+    # Read world name from txt file in order to name the zip file with it
     txt = os.path.join(world,'levelname.txt')
     with open(txt, 'r') as file:
         world_name = file.read()
@@ -40,7 +33,5 @@ for world in worlds:
     counter += 1
     output_path = os.path.join(output_base, f'{world_name}.mcworld')
     
-    
     zip_folder(world, output_path)
     print('Saved',output_path)
-
